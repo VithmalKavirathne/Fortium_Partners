00a3d31b6a778604c762846108ff99d85d64bc15
VithmalKavirathne <vithakavirathne@gmail.com>
2025-04-29 13:17:40 +0530

    controller updates

--- src/main/java/edu/icet/controller/EmployeeController.java
+++ src/main/java/edu/icet/controller/EmployeeController.java
@@ -1,10 +1,27 @@
 package edu.icet.controller;
 
+import edu.icet.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping
+@RequestMapping("/api/employees")
+
 public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService){
+        this.employeeService=employeeService;
+    }
+
+    @GetMapping
+    public List<Employee>getAllEmployees;
 
 }
